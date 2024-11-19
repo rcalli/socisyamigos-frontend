77cae51fb8a4c4cda6d6c7194f75c13a549ee82b
TheMauCX <mau.channelxd@gmail.com>
2024-11-19 15:34:34 -0500

    requisito seguridad

---
+++ src/app/features/mantener/requisito/requisito.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RequisitoComponent } from './requisito.component';
+
+const routes: Routes = [
+  { path: '', component: RequisitoComponent }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class RequisitoModule {}
