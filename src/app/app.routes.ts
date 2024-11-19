d8106ef9fbfa145b3488274de465fbc3d783fb7a
TheMauCX <mau.channelxd@gmail.com>
2024-11-18 22:48:49 -0500

    crud evaluacion, seguridad con token duro xd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EvaluacionComponent } from './components/mantener/evaluacion/evaluacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        title:'home'
+    },
+    {
+        path:'evaluacion',
+        component:EvaluacionComponent,
+        title:'evaluacion'
+    },
+    {
+        path:'**',
+        redirectTo:'',
+        pathMatch:'full'
+    }
+];
